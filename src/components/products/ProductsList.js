809edf4c5f516ad77163c19bc4fd366e1549b611
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-01-06 16:19:37 -0500

    Add a text box to filter products by name

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,4 +1,6 @@
-import { React, useEffect } from 'react';
+import {
+  React, useState, useEffect, useMemo,
+} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductsItem from './ProductsItem';
 import { getProducts } from '../../redux/products/ProductsSlice';
@@ -6,6 +8,14 @@ import { getProducts } from '../../redux/products/ProductsSlice';
 const ProductsList = () => {
   const dispatch = useDispatch();
   const { productItems } = useSelector((store) => store.products);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredProducts = useMemo(
+    () => productItems.filter(
+      (product) => product.nom_producto.toLowerCase().includes(searchQuery.toLowerCase()),
+    ), [productItems, searchQuery],
+  );
+
   useEffect(() => {
     if (productItems.length === 0) {
       dispatch(getProducts());
@@ -14,7 +24,13 @@ const ProductsList = () => {
 
   return (
     <div className="product-listItems">
-      {productItems.map((product) => (
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Busca productos..."
+      />
+      {filteredProducts.map((product) => (
         <ProductsItem
           key={product.id}
           productId={product.id}
